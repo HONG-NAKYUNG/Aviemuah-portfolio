9fe1fb9b83ad638c0cf0c13ba1c676e567557056
HONG-NAKYUNG <ghdskrud4046@gmail.com>
2024-12-13 17:30:37 +0900

    수원스타필드 포트폴리오 업데이트

---
+++ js/common.js
@@ -0,0 +1,12 @@
+$(function () {
+  let currentIndex = 0;
+
+  setInterval(function () {
+    let nextIndex = (currentIndex + 1) % 3;
+
+    $(".slider").eq(currentIndex).fadeOut(1200);
+    $(".slider").eq(nextIndex).fadeIn(1200);
+
+    currentIndex = nextIndex;
+  }, 4000);
+});
